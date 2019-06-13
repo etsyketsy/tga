18d96aab8d1406ccdc5824a6856b0cb95af45cae
Sarah Rabah <sarahmrabah@gmail.com>
2019-06-13 21:57:02 +0200

    Uncommenting Token Authentication from DRF section in settings.py

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
